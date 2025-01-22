d9113e765f6c1de21c3ee98ebe08ca84c00bcefd
Muhammad Ibrahim <mibrahimm1338@gmail.com>
2025-01-22 11:50:16 +0500

    card images in admin updated

--- client/src/components/Admin-Page/AdminDashboard.jsx
+++ client/src/components/Admin-Page/AdminDashboard.jsx
@@ -9,10 +9,10 @@ function AdminDashboard() {
 <div className="cards-container flex flex-row gap-10 flex-wrap mb-5 ">
         
         <Card number={2} title={'Doctors'} imageSrc={'https://png.pngtree.com/png-clipart/20230918/ourmid/pngtree-photo-men-doctor-physician-chest-smiling-png-image_10132895.png'} to={'/admin/doctors'}/>
-        <Card number={3} title={'Receptionists'} imageSrc={'https://png.pngtree.com/png-clipart/20230918/ourmid/pngtree-photo-men-doctor-physician-chest-smiling-png-image_10132895.png'} to={'/admin/receptionists'}/>
-        <Card number={4} title="Pharmacists" imageSrc={'https://png.pngtree.com/png-clipart/20230918/ourmid/pngtree-photo-men-doctor-physician-chest-smiling-png-image_10132895.png'} to={'/admin/pharmacists'}/>
-        <Card number={9} title={'Radiologists'} imageSrc={'https://png.pngtree.com/png-clipart/20230918/ourmid/pngtree-photo-men-doctor-physician-chest-smiling-png-image_10132895.png'} to={'/admin/doctors'}/>
-        <Card number={9} title={'Laboratorists'} imageSrc={'https://png.pngtree.com/png-clipart/20230918/ourmid/pngtree-photo-men-doctor-physician-chest-smiling-png-image_10132895.png'} to={'/admin/doctors'}/>
+        <Card number={3} title={'Receptionists'} imageSrc={'https://cdn-icons-png.flaticon.com/256/5703/5703682.png'} to={'/admin/receptionists'}/>
+        <Card number={4} title="Pharmacists" imageSrc={'https://png.pngtree.com/png-clipart/20231020/original/pngtree-pharmacy-and-medicine-and-pharmacist-png-image_13373034.png'} to={'/admin/pharmacists'}/>
+        <Card number={9} title={'Radiologists'} imageSrc={'https://static.vecteezy.com/system/resources/thumbnails/048/346/341/small_2x/doctor-examining-an-x-ray-analyzing-patient-medical-images-png.png'} to={'/admin/doctors'}/>
+        <Card number={9} title={'Laboratorists'} imageSrc={'https://png.pngtree.com/png-clipart/20240905/original/pngtree-lab-assistant-and-technician-png-image_15939034.png'} to={'/admin/doctors'}/>
 
 
       </div>
--- client/src/components/Dashboard/Card.jsx
+++ client/src/components/Dashboard/Card.jsx
@@ -6,7 +6,7 @@ function Card({ number, title, imageSrc, to }) {
 
     <Link to={to}>
     <div className="card-container flex justify-between items-stretch rounded-lg shadow-md bg-white overflow-hidden h-[200px] w-[285px] border-b-8 border-blue-600">
-        <div className="card-number-title flex flex-col p-6 gap-2"> {/* Adjusted gap for better spacing */}
+        <div className="card-number-title flex flex-col p-6 gap-2"> 
             <span className="card-number text-4xl text-blue-600 font-bold">{number}</span>
             <span className="card-title">{title}</span>
         </div>
